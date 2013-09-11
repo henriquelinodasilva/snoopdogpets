90e3f31879f31edd61359b439eb02d59ca2b1d84
Johnny <johnny_gabriel@outlook.com>
2013-09-11 16:50:53 -0300

    Adicionado pagina inicial

---
+++ index.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Administrador do sistema</title>
+		<link href="http://snoopdogpets.p.ht/css/bootstrap.css" rel="stylesheet">
+		<link href="http://snoopdogpets.p.ht/css/style.css" rel="stylesheet">
+		<script src="http://snoopdogpets.p.ht/js/jquery-1.10.2.min.js"></script>
+		<script src="http://snoopdogpets.p.ht/js/bootstrap.js"></script>
+	</head>
+	
+	<body style="padding: 30px;">
+		<div class="home_menu_center">
+			<ul class="thumbnails">
+				  <li class="span4">
+					<div class="thumbnail">
+					  <img class="index_menu_img" src="http://snoopdogpets.p.ht/img/home_funcionario.jpg" alt="">
+					  <div class="caption">
+						<h3>Funcionários</h3>
+						<p>Gerencie os dados de acesso dos seus funcionários.</p>
+						<p>
+							<a href="/funcionario" target="_blank" class="btn btn-primary">Adicionar</a> 
+					      	<a href="/funcionario/consulta" target="_blank" class="btn">Consultar</a>
+						</p>
+					  </div>
+					</div>
+				  </li>
+				  <li class="span4">
+					<div class="thumbnail">
+					  <img class="index_menu_img" src="http://snoopdogpets.p.ht/img/home_fornecedores.jpg" alt="">
+					  <div class="caption">
+						<h3>Fornecedores</h3>
+						<p>Gerencie e consulte os dados dos seus fornecedores.</p>
+						<p>
+							<a href="/fornecedor" target="_blank" class="btn btn-primary">Adicionar</a> 
+						 	<a href="/fornecedor/consulta" target="_blank" class="btn">Consultar</a>
+						</p>
+					  </div>
+					</div>
+				  </li>
+				  <li class="span4">
+					<div class="thumbnail">
+					  <img class="index_menu_img" src="http://snoopdogpets.p.ht/img/home_vendas.jpg" alt="">
+					  <div class="caption">
+						<h3>Vendas</h3>
+						<p>Consulte os dados das vendas.</p>
+						<p>
+						 	<a href="/consulta" target="_blank" class="btn">Consultar</a>
+						</p>
+					  </div>
+					</div>
+				  </li>
+				</ul>
+			</div>
+	</body>
+</html>
+	
